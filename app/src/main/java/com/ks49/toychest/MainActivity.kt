ab96c617f2b7ebe744199a648cb01af057d0ef5f
AJ Myrick <ajmyrick02@gmail.com>
2020-02-18 00:38:17 -0500

    Initial commit

---
+++ app/src/main/java/com/ks49/toychest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ks49.toychest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
